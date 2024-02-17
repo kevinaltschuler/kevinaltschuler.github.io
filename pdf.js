02eca94978f5725cd5de9aa07d1f4bfee9b4ea20
kevin altschuler <altschuler@dropbox.com>
2024-02-16 20:37:59 -0500

    wip

---
+++ pdf.js
@@ -0,0 +1,49 @@
+import * as pdfjsDist from "./node_modules/pdfjs-dist/build/pdf.min.mjs";
+import * as pdfjsWorker from "./node_modules/pdfjs-dist/build/pdf.worker.mjs";
+
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+var url = "./img/resume10.pdf";
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+// The workerSrc property shall be specified.
+pdfjsDist.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+// Asynchronous download of PDF
+var loadingTask = pdfjsDist.getDocument(url);
+loadingTask.promise.then(
+  function (pdf) {
+    console.log("PDF loaded");
+
+    // Fetch the first page
+    var pageNumber = 1;
+    pdf.getPage(pageNumber).then(function (page) {
+      console.log("Page loaded");
+
+      var scale = 4;
+      var viewport = page.getViewport({ scale: scale });
+
+      // Prepare canvas using PDF page dimensions
+      var canvas = document.getElementById("the-canvas");
+      var context = canvas.getContext("2d");
+      canvas.width = viewport.width;
+      canvas.style.width = "100%";
+      canvas.height = viewport.height;
+      canvas.style.height = "100%";
+
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport,
+      };
+      var renderTask = page.render(renderContext);
+      renderTask.promise.then(function () {
+        console.log("Page rendered");
+      });
+    });
+  },
+  function (reason) {
+    // PDF loading error
+    console.error(reason);
+  }
+);
